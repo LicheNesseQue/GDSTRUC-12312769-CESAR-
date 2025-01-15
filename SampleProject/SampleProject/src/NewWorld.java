1f7e3285dc733410fef4dcdfeb3feeddf2df42ce
LicheNesseQue <159520960+LicheNesseQue@users.noreply.github.com>
2025-01-15 09:20:11 +0800

    Sample code for scan input

---
+++ SampleProject/SampleProject/src/NewWorld.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class NewWorld {
+        public static void main(String[] args){
+
+            System.out.println("Hello world!");
+            System.out.println("Enter your name: ");
+
+            Scanner inputScanner = new Scanner(System.in);
+            String name = inputScanner.nextLine();
+
+            System.out.println("Hello " + name + "!");
+        }
+
+
+}
